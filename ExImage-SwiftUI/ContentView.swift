1f94c27591e1001d868f6be2f247294225bfc259
jake <palatable7@naver.com>
2022-07-27 20:54:13 +0900

    Implement

--- ExImage-SwiftUI/ContentView.swift
+++ ExImage-SwiftUI/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var body: some View {
+    ScrollView {
+//      Image("book")
+//        .resizable()
+//        .scaledToFill()
+//
+//      Image("book")
+//        .resizable()
+//        .scaledToFit()
+//
+//      Image("book")
+//        .resizable()
+//        .aspectRatio(contentMode: .fit)
+//
+//      Image("book")
+//        .resizable()
+//        .aspectRatio(contentMode: .fill)
+      
+//      Image("book")
+//        .resizable()
+//        .scaledToFit()
+//        .clipShape(Circle())
+//
+//      Image("book")
+//        .resizable()
+//        .scaledToFit()
+//        .clipShape(Circle().inset(by: 20))
+      
+      Image("book")
+        .resizable()
+        .renderingMode(.template)
+        .border(.black)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
