f778f5b763ad6964fdd0513c23c98d59f258bbfb
jake <palatable7@naver.com>
2022-07-27 19:45:19 +0900

    Initial Commit

---
+++ ExImage-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExImage-SwiftUI
+//
+//  Created by 김종권 on 2022/07/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExImage-SwiftUI/ExImage_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExImage_SwiftUIApp.swift
+//  ExImage-SwiftUI
+//
+//  Created by 김종권 on 2022/07/27.
+//
+
+import SwiftUI
+
+@main
+struct ExImage_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
